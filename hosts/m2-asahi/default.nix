9079cc368681df04c8f495ca2565014443a27958
Snoilt <paul@oellers.net>
2026-09-04 11:30:12 +0200

    feat: fairydust test

--- hosts/m2-asahi/default.nix
+++ hosts/m2-asahi/default.nix
@@ -1,9 +1,44 @@
 {
+  config,
   inputs,
   lib,
   pkgs,
   ...
-}: {
+}: let
+  asahiPkgs = config.hardware.asahi.pkgs;
+  fairydustVersion = "7.1.12";
+  fairydustKernel = asahiPkgs.buildLinux {
+    inherit lib;
+    inherit (asahiPkgs) stdenv;
+    pname = "linux-asahi-fairydust";
+    version = fairydustVersion;
+    modDirVersion = fairydustVersion;
+    extraMeta.branch = "7.1";
+    src = asahiPkgs.fetchFromGitHub {
+      owner = "AsahiLinux";
+      repo = "linux";
+      rev = "b8810ad6442699f610984f3eceea2e3234a50b77";
+      hash = "sha256-FTns+uaqYbCYSsH0y7ypTHUzZs3GAM08vRwHGy2Tozc=";
+    };
+    kernelPatches = [
+      {
+        name = "Asahi config";
+        patch = null;
+        structuredExtraConfig = with lib.kernel; {
+          ARM64_16K_PAGES = yes;
+          ARM64_MEMORY_MODEL_CONTROL = yes;
+          ARM64_ACTLR_STATE = yes;
+          APPLE_WATCHDOG = yes;
+          APPLE_M1_CPU_PMU = yes;
+          HID_APPLE = module;
+          APPLE_PMGR_MISC = yes;
+          APPLE_PMGR_PWRSTATE = yes;
+        };
+        features.rust = true;
+      }
+    ] ++ config.boot.kernelPatches;
+  };
+in {
   imports = [
     ./desktop.nix
     ./displaylink.nix
@@ -18,6 +53,9 @@
   boot.loader.systemd-boot.configurationLimit = 7;
   boot.loader.efi.canTouchEfiVariables = lib.mkForce false;
   boot.kernelParams = ["appledrm.show_notch=1"];
+  # Asahi's fairydust branch provides experimental USB-C DisplayPort support.
+  boot.kernelPackages = lib.mkForce (asahiPkgs.linuxPackagesFor fairydustKernel);
+  boot.kernelModules = ["typec_displayport"];
   hardware.asahi.enable = true;
   networking.firewall.enable = lib.mkForce true;
   networking.networkmanager.wifi.backend = "iwd";
@@ -26,7 +64,6 @@
   services.gnome.gnome-keyring.enable = true;
   programs.firefox.enable = true;
  
-
   nix.settings.cores = 4;
   
   my.username = "paul";
